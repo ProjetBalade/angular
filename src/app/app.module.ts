4700fc797d2a7bacd1fe8ea9eb993a74233c6ed1
Andre.Tyran <feefpllogo@gmail.com>
2021-12-22 12:59:39 +0100

    formulaires

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { AjoutChienComponent } from './ajout-chien/ajout-chien.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    AjoutChienComponent
   ],
   imports: [
     BrowserModule
